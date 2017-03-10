ca998ed8ecd938c645a5809cf8e0c605b9dda677
thefirstcommonname <bakayarotinker@gmail.com>
2017-03-10 16:58:07 -0500

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,8 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [{
+  text: "First test todo"
+}, {
+  text: "Second test todo"
+}];
+
 beforeEach(function(done) {
   Todo.remove({}).then(function() {
+    return Todo.insertMany(todos);
+  }).then(function() {
     done();
   })
 });
@@ -25,7 +33,7 @@ describe('POST /todos', function() {
         if (err) {
           return done(err);
         }
-        Todo.find().then(function(todos) {
+        Todo.find({text}).then(function(todos) {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -45,11 +53,23 @@ describe('POST /todos', function() {
         return done(err);
       }
       Todo.find().then(function(todos) {
-        expect(todos.length).toBe(0);
+        expect(todos.length).toBe(2);
         done()
       }).catch(function(e) {
         done(e);
       })
     })
+  });
+});
+
+describe('GET /todos', function() {
+  it('should get all todos', function(done){
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(function(res){
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   });
 });
