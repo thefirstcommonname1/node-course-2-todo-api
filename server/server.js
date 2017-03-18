7d4007aac800435e1e14b436bcf51c7063062254
thefirstcommonname <bakayarotinker@gmail.com>
2017-03-18 14:01:09 -0400

    Add GET users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -113,6 +114,12 @@ app.post('/users', function(req, res) {
   }).catch(function(e) {
     res.status(400).send(e);
   });
+});
+
+
+
+app.get('/users/me', authenticate, function(req, res) {
+  res.send(req.user);
 });
 
 app.listen(port, function() {
